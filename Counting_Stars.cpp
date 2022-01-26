bf76f302c0c31702bd801baab23b49395e17c495
Jorin-Post <jorin101@hotmail.nl>
2022-01-26 14:53:40 +0100

    V0.0

---
+++ Counting_Stars.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std::endl;
+using std::cin;
+using std::cout;
+
+int main(){
+    cout << "Hello world" << endl;
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Counting_stars
+Take a pictrure of Hubble Telescope and count the stars in the picture.
