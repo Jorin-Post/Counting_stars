9ee33ce0126039181ce6fb5d567f2b65f501293f
Jorin-Post <jorin101@hotmail.nl>
2022-02-05 17:45:58 +0100

    V2

--- Counting_Stars.cpp
+++ Counting_Stars.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <string>
+#include <string.h>
 
 using namespace std;
 
@@ -37,61 +37,11 @@ int count_stars(){
     }
     return Stars; // return the amount of stars
 }
+
 int count_pix(){
-    ifstream nimage;
-    nimage.open("newapollo.ppm");
-
-    string type = "", w="", h="", RGB="";
-    nimage >> type;
-    nimage >> w;
-    nimage >> h;
-    nimage >> RGB;
-    int width = stoi(w), height = stoi(h);
-    int px = width * height;
-
-    cout << "Type of PPM: " << type << endl;
-    cout << "Amount of Color: "<< RGB << endl;
-    cout << "Amount of px: " << px << endl;
-    
-    int x = 0, y = 0, xval = 0;
-
-    string red ="", green="", blue="";
-    int r=0, g=0, b=0, px_count = 0;
-    while (!nimage.eof())
-    {
-        nimage >> red;
-        nimage >> green;
-        nimage >> blue;
-
-        r = stoi(red);
-        g = stoi(green);
-        b = stoi(blue);
-
-        if (r == 255 && g == 255 && b == 255)
-        {
-            vx.push_back(x);
-            vy.push_back(y);
-            px_count++;
-        }
-
-        
-        if (x == width){
-            y++;
-            xval = x;
-            x = 0;
-        } 
-        x++;
-    }
-    cout << "X count: " << xval << endl;
-    cout << "Y count: " << y << endl;
-    return px_count;
-}
-void black_white(){
     ifstream image;
-    ofstream nimage;
 
-    image.open("apollo.ppm");
-    nimage.open("newapollo.ppm");
+    image.open("hubble.ppm", ifstream::binary);
 
     // get headers
     string type = "", width="", height="", RGB="";
@@ -99,41 +49,38 @@ void black_white(){
     image >> width;
     image >> height;
     image >> RGB;
+    cout << type << '\t' << width << '\t' << height << '\t' << RGB << endl;
 
-    // Make header for new pic
-    nimage << type << endl;
-    nimage << width << " " << height << endl;
-    nimage << RGB << endl;
-
-    string red ="", green="", blue="";
-    int r=0, g=0, b=0;
+    uint8_t red, green, blue;
+    int x=0, y=0, px_count=0, px_white=0, w = stoi(width), h = stoi(height);
     while (!image.eof()) // goes by pix till no left.
     {
         image >> red;
         image >> green;
         image >> blue;
 
-        r = stoi(red);
-        g = stoi(green);
-        b = stoi(blue);
+        int r = (int)(red), g = (int)(green), b = (int)(blue);
 
         if (r > 200 && g > 200 && b > 200){ // filter vall
-            r = 255, g = 255, b = 255;
-        }else{
-            r = 0, g = 0, b = 0;
+            vx.push_back(x);
+            vy.push_back(y);
+            px_white++;
         }
-
-        nimage << r << " " << g << " " << b << endl;
+        px_count++;
+        if (x < w+1)
+            x++;
+        else 
+            y++;
     }
     image.close();
-    nimage.close();
+    return px_count, px_white;
 }
+
 int main(){
-    int px_count = 0;
-    black_white();
-    px_count = count_pix();
+    int px_count, px_white = count_pix();
     int stars = count_stars();
-    cout << "Px that are white: " << px_count << endl;
+    cout << "Px that are in pic: " << px_count << endl;
+    cout << "Px that are white: " << px_white << endl;
     cout << "There are " << stars << " stars in the pic!" << endl;
     return 0;        
 }
